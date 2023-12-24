32113528324bd49a377f01dd4ce52d55bda766fa
sfybb <129160899+sfybb@users.noreply.github.com>
2023-12-24 16:44:57 +0100

    added usage of tempalte strings for logging

--- src/logging.ts
+++ src/logging.ts
@@ -16,31 +16,31 @@ function $log(log_lvl: LogLevel, msg: string) {
 
 
 function $log_trace(msg: string) {
-    $log!(LogLevel.TRACE,    "[TRACE]    "+msg)
+    $log!(LogLevel.TRACE,    `[TRACE]    ${msg}`)
 }
 function $log_debug(msg: string) {
-    $log!(LogLevel.DEBUG,    "[DEBUG]    "+msg)
+    $log!(LogLevel.DEBUG,    `[DEBUG]    ${msg}`)
 }
 function $log_info(msg: string) {
-    $log!(LogLevel.INFO,     "[INFO]     "+msg)
+    $log!(LogLevel.INFO,     `[INFO]     ${msg}`)
 }
 function $log_warn(msg: string) {
-    $log!(LogLevel.WARNING,  "[WARNING]  "+msg)
+    $log!(LogLevel.WARNING,  `[WARNING]  ${msg}`)
 }
 function $log_err(msg: string) {
-    $log!(LogLevel.ERROR,    "[ERROR]    "+msg)
+    $log!(LogLevel.ERROR,    `[ERROR]    ${msg}`)
 }
 
 function $log_crit(msg: string, addInfo: string) {
-    game?.print("[FACTORIO CODEX] [CRITICAL] [color=orange]" + msg + ". Details are in the log file. If this persists try running [/color][color=cyan]/fc-rebuild-all[/color]", {r: 0.5})
-    log("[CRITICAL] " + msg)
-    log("[INFO] " + addInfo)
-    log("[TRACEBACK] " + debug.traceback())
+    game?.print(`[FACTORIO CODEX] [CRITICAL] [color=orange]${msg}. Details are in the log file. If this persists try running [/color][color=cyan]/fc-rebuild-all[/color]`, {r: 0.5})
+    log(`[CRITICAL] ${msg}`)
+    log(`[INFO] ${addInfo}`)
+    log(`[TRACEBACK] ${debug.traceback()}`)
 }
 
 function $log_crit_raw(msg: string) {
-    game?.print("[FACTORIO CODEX] [CRITICAL] [color=orange]" + msg, {r: 0.5})
-    log("[CRITICAL] " + msg)
+    game?.print(`[FACTORIO CODEX] [CRITICAL] [color=orange]${msg}`, {r: 0.5})
+    log(`[CRITICAL] ${msg}`)
 }
 
 function $get_player_string(pID: PlayerIndex): string {
